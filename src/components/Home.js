e4a96abc460e3ac75ba0ee26fb5edc8abcd938b1
CRgarcia15 <cg58708@gmial.com>
2025-05-12 13:56:08 -0400

    update: beginings of giving the finishing toucher to the project.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-/*
-make the home page look modern, add button to login, make a sample project view to 
-show user what it will look like.
-*/
 function Home () {
 
     return (
-      <div className="w-1/2 text-sm font-semibold border rounded-lg shadow-md mx-auto focus:outline-none focus:border-green-600 text-7xl">
-        <h1>HOME</h1>
+      <div className="w-1/2 text-sm border rounded-lg shadow-md mx-auto focus:outline-none focus:border-green-600 text-7xl grid h-56 grid-cols-2 content-stretch gap-6 md:content-around">
+        <div className="border-2 border-black rounded-md">
+            <h3 className="text-lg font-bold ">"Order is the foundation upon which great things are built."</h3>
+            <p className="text-base"> SprintTrack was created with the purpose of helping those with a busy schedule organize their
+                agenda in a modern and effective way.
+            </p>
+            <p className="text-base font-semibold">Let's start creating a new horizon together </p>
+            <button className="text-lg font-bold border-2 border-black rounded-md">Sign-up</button>
+        </div>
+        <div className="border-2 border-black rounded-md"> 
+          <h3 className="text-base">Sample Title 2 </h3>
+        </div>
       </div>
     )
 }
